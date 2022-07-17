69a92e8f09e6f76719035d06bed23bf985629505
eyobodega <76756721+eyobodega@users.noreply.github.com>
2022-07-16 22:44:32 -0400

    Add files via upload

---
+++ atmega328p_code/atmega_c+isr+asm.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=atmega_c_isr_asm.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/atmega_c_isr_asm.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=atmegac+isr+asm.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/atmegac+isr+asm.x.tar
